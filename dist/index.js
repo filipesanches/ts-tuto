1e7423fd9c2d47d801a89af82567d6a98a117938
Filipe Sanches <53585862+filipesanches@users.noreply.github.com>
2024-03-20 14:04:18 +0000

    TS

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(a, b) {
+    const c = a + b;
+    return c;
+}
+sum('1', 2);
---
+++ src/ts/index.ts
@@ -0,0 +1,6 @@
+function sum(a: any, b: any): string {
+  const c = a + b;
+  return c;
+}
+
+sum('1', 2);
